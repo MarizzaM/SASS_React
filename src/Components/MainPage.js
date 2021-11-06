68058a9ca7bf5c4b377f4876b17dc3bdc3c541ec
Pavlova Marina <marizza.mil89@gmail.com>
2021-11-06 02:08:20 +0200

    01 Create React App

---
+++ src/Components/MainPage.js
@@ -0,0 +1,76 @@
+
+import React from "react";
+
+function MainPage() {
+    return (
+        <div className="mainPage">
+
+            <div className="lead-banner">
+                <img src="img/banner-1.png" />
+                <div className="banner-content">
+                    <div className="wrapper">
+                        <span className="title">Check our video</span>
+                        <span className="sub-title">Learning in 6 weeks</span>
+                    </div>
+                </div>
+            </div>
+        
+            <section className = "services">
+                <div className="wrapper">
+                    <h1>Services</h1>
+                    <div >
+                        <div className = "items">
+                            <img src="img/icon-1.png" />
+                            <a href="#">Recording<br />services</a>
+                        </div>
+                        <div className = "items">
+                            <img src="img/icon-2.png" />
+                            <a href="#">Get a printed<br />certificate</a>
+                        </div>
+                        <div className = "items">
+                            <img src="img/icon-3.png" />
+                            <a href="#">Lessons via<br />Skype</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="lessons-banner">
+                <img src="img/banner-2.png" />
+                <div className="banner-content">
+                    <div className="wrapper">
+                        <span className="title">Our Lessons</span>
+                        <span className="sub-title">Are Easy</span>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <section className="projects">
+                <div className="wrapper">
+                    <h1>Our Projects</h1>
+                    <ul>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                        <li><img src="http://placehold.it/150x150" /></li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    );
+}
+  
+  export default MainPage;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+function Navbar() {
+    return (
+      <div className="navbar">
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/lessons">Lessons</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Navbar;
